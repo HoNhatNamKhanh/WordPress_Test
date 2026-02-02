29424f03f6164e3899931252f213e7377ce12d19
khanh.ho <honhatnamkhanh10b1@gmail.com>
2026-02-02 21:23:03 +0700

    Update for single post

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header('header')?>
 
-<?php get_template_part('./includes/section' ,'content') ?>
+<?php get_template_part('./includes/section' ,'postcontent') ?>
 
 <?php get_footer('footer')?>  
